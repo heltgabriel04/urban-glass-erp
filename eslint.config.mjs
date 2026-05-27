92527f3f5baf47afe5964e4f99251c381ca05a9f
Gabriel <heltgabriel@gmail.com>
2026-05-27 16:41:51 -0300

    fix: simplifica eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextConfig from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
 
 const eslintConfig = defineConfig([
-  ...nextConfig,
-  ...nextTs,
   globalIgnores([
     ".next/**",
     "out/**",
